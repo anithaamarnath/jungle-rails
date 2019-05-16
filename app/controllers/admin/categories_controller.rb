6d095570e4e418f619d000fd0e348693be10792e
anithaamarnath <anithaamarnath04@gmail.com>
2019-05-16 01:27:13 +0000

    admin categories functionality is added

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,23 @@
+class Admin::CategoriesController < ApplicationController
+
+  def index
+   @category = Category.all.order(created_at: :desc)
+
+  end
+  def new
+    @categorys = Category.new
+  end
+
+  def create
+    @categorys = Category.new(category_params)
+
+    if @categorys.save
+      redirect_to [:admin, :categories], notice: 'New Categories created!'
+    else
+      render :new
+    end
+  end
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
